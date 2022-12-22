2e36226ff1f1e63423b3da14930edb7bd8732005
NataliaDC <nataliadiazc@hotmail.com>
2022-12-21 20:04:18 -0500

    Orientacion

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:parcial_dispositivos/windows/login.dart';
 import 'package:parcial_dispositivos/windows/principal.dart';
 import 'package:parcial_dispositivos/windows/registro.dart';
@@ -18,6 +19,10 @@ class MyApp extends StatefulWidget {
 class _MyHomePageState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData().copyWith(
